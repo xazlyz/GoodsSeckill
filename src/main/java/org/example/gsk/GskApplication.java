d44984283172c536e1bc2b84a9ff2c165085ba01
yi fan <2434457965@qq.com>
2024-09-02 15:59:15 +0800

    Changes

---
+++ src/main/java/org/example/gsk/GskApplication.java
@@ -0,0 +1,13 @@
+package org.example.gsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (GskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gsk/GskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
