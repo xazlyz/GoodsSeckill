c34a2e2acc0847f510bbd9e5d743f7107c391032
yi fan <2434457965@qq.com>
2024-09-02 15:59:15 +0800

    依赖完善

--- src/main/java/org/example/gsk/controller/BusinessesManController.java
+++ src/main/java/org/example/gsk/controller/BusinessesManController.java
@@ -1,8 +1,17 @@
 package org.example.gsk.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/businessesman")
 public class BusinessesManController {
+
+    @PostMapping
+    public String run()
+    {
+        return "run";
+    }
 
 }
